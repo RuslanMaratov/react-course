078c99880a38830b47f0024ef5e13c5c961b9f5e
Ruslan Maratov <ruslanmaratov22@mail.ru>
2023-10-16 14:24:31 +0300

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+        alt="logo"
+        src="https://w7.pngwing.com/pngs/70/772/png-transparent-vimeo-computer-icons-logo-short-film-vitag-svg-miscellaneous-television-text-thumbnail.png"
+      />
+    </header>
+  );
+}
---
+++ src/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div className="profile">
+      <img
+        className="profile-img"
+        alt="profile-img"
+        src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+      />
+      Main content
+    </div>
+  );
+}
